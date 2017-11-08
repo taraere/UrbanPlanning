0c904c6464e9a3a66b6cf11ee0498d212065b168
Jos Feenstra <feenstrajos@gmail.com>
2017-11-08 12:10:22 +0100

    House class in its primal form

--- printSquare.py
+++ printSquare.py
@@ -12,21 +12,59 @@ NOTE    this thing is an example of how to use helpers
 
 from helpers import *
 
+class House:
 
+
+    def __init__(self, aType, aCoord, ExtraRings):
+        self.type = aType
+        self.origin = aCoord
+        self.rings = ExtraRings
+        self.update()
+
+    def update(self):
+        self.coord = self.origin
+
+        # calculate additional geometric information, based on
+
+    def move(self, vector):
+        # example vector = (6, -1)
+        self.origin = tuple(map(sum, zip(self.origin, vector))
+
+        # change other values with this change
+        self.update()
+
+    def moveTo(self, newCoord):
+        self.origin = newCoord
+
+        # change other values with this change
+        self.update()
 
 def main():
     ht = initHouseTypes(20)
 
     # TEST print ring values
-    for houseType in ht:
-        houseType.printRingInfo()
+    #for houseType in ht:
+        #houseType.printRingInfo()
 
+    # example house object
+    hType = ht[1]
+    coordinate = (50, 50)
+    ringsToAdd = 3
+    someHouse = House(hType, coordinate, ringsToAdd)
 
-    # # what i want to make:
-    #                       houseType, houselocation, number of extra rings
-    # houseInstance = House(ht[1], (50,50), 3)
-    # print(houseInstance.type.name)
-    # >>> "Bungalow"
+    # test
+    print(someHouseInstance.type.name)
+    print(someHouseInstance.coord)
+
+    # example moveto
+    someCoord = (23, 10)
+    someHouse.moveto()
+    print(someHouse.coord)
+
+    # example move
+    somevector = (-100, -100)
+    somehouse.move(someVector)
+    print(someHouse.coord)
 
 if __name__ == "__main__":
     main()
