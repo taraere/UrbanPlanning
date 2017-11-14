26735e816bd02db1e4732a8f758dfd560041b9ae
Jos Feenstra <feenstrajos@gmail.com>
2017-11-14 11:49:31 +0100

    Finished correct helpers.py implementation

--- printSquare.py
+++ printSquare.py
@@ -12,32 +12,125 @@ NOTE    this thing is an example of how to use helpers
 
 from helpers import *
 
+# choose 0, 1 or 2 to get 20, 40 or 60 houses
+SELECTED_HOUSE_COUNT = HOUSE_COUNT[0]
+
+"""
+build a hardcoded sollution
+"""
 def main():
     ht = initHouseTypes(20)
 
-    # TEST print ring values
-    #for houseType in ht:
-        #houseType.printRingInfo()
-
-    # example house object
+    # example house object values
     hType = [ht[0], ht[2], ht[0], ht[1], ht[2], ht[0], ht[2], ht[0], ht[1], ht[2]]
     coordinate = [(50, 50), (64, 21), (54, 50), (84, 97), (12, 23), (45, 56), (100, 100), (30, 30), (200, 54)]
     ringsToAdd = [3, 3, 6, 4, 7, 8, 9, 3, 0, 0]
+
+    # make a map
+    map1 = Map()
+
+    # fill the map
+    for i in range(9):
+        map1.addHouse(hType[i], coordinate[i], ringsToAdd[i])
+
+
+    something = map1.house[1].boundary.height
+    print(something)
+
+    # draw the map
+    map1.plot()
+
+
+
+
+
+"""
+build a random sollution
+"""
+def main2():
+    housetypes = initHouseTypes(20)
+
+    # generate hosue parameteres
+    housetypelist = []
+    for housetype in housetypes:
+        n = round(housetype.frequency * SELECTED_HOUSE_COUNT)
+        housetypelist += [housetype] * n
+
+    # houstypelist = shuffle(housetypelist)
+
+    # make a map
+    map1 = Map()
+
+    # add houses to map
+    for i in range(SELECTED_HOUSE_COUNT):
+
+        # create other parameters
+        coordinate = (randint(0, AREA[0]-1), randint(0, AREA[1]-1))
+        ringsToAdd = randint(1, 12)
+
+        # add house
+        map1.addHouse(housetypelist[i], coordinate, ringsToAdd)
+
+    # draw the map
+    map1.plot()
+
+
+"""
+build a correct random sollution
+"""
+def main3():
+    housetypes = initHouseTypes(20)
+
+    # generate hosue parameteres
+    housetypelist = []
+    for ht in housetypes:
+        n = round(ht.frequency * SELECTED_HOUSE_COUNT)
+        housetypelist += [ht] * n
+
+    houstypelist = shuffle(housetypelist)
+    print(housetypelist)
+
+    # make a map
+    map1 = Map()
+
+    # add houses to map
+    for i in range(SELECTED_HOUSE_COUNT):
+
+        # create other parameters
+        coordinate = (randint(0, AREA[0]-1), randint(0, AREA[1]-1))
+        ringsToAdd = randint(1, 12)
+
+        # add house
+        map1.addHouse(housetypelist[i], coordinate, ringsToAdd)
+
+    # draw the map
+    map1.plot()
+
+
+
+
+def main4():
+    housetypes = initHouseTypes(20)
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
+
+
+
+
 
     # # example move
     # someVector = (100, 100)
     # someHouse.move(someVector)
     # print(someHouse.coord)
-
-    houses = []
-    for i in range(len(coordinate)):
-        houses.append(House(hType[i], coordinate[i], ringsToAdd[i]))
-
-    # draw the board
-    drawAll(houses)
-
-if __name__ == "__main__":
-    main()
 
 
 # questions about the case:
